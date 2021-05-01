c8ffe6ca3e88d2f0eb57ec27bcc3be9229868732
anurag9261 <anurag.webcreta@gmail.com>
2021-05-01 12:18:38 +0530

    changes done

--- resources/views/admin/users/edituser.blade.php
+++ resources/views/admin/users/edituser.blade.php
@@ -68,7 +68,7 @@
                         <br>
                         <div class="row">
                             <div class="col-md-5">
-                                <label for="mobno">Phone Number</label>
+                                <label for="mobno">Mobile Number</label>
                                 <input type="number" class="form-control @error('mobno') is-invalid @enderror"
                                     name="mobno" value="{{$profile->mobno}}">
                                 @error('mobno')
